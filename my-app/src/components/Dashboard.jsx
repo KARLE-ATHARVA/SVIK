801e609f27fc90abca952f0d4e89a31403b24a2f
fernandesjasmine <160141215+fernandesjasmine@users.noreply.github.com>
2025-07-29 19:38:46 +0530

    Update Dashboard.jsx

--- my-app/src/components/Dashboard.jsx
+++ my-app/src/components/Dashboard.jsx
@@ -5,15 +5,22 @@ import Topbar from './Topbar';
 import MainContent from '../pages/LandingPage';
 
 export default function Dashboard() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(prev => !prev);
+  };
 
   return (
-    <div className="flex h-screen bg-gray-100 overflow-hidden">
-      <Sidebar collapsed={collapsed} />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Topbar collapsed={collapsed} setCollapsed={setCollapsed} />
-        <MainContent />
+    <div className={darkMode ? "dark flex h-screen overflow-hidden" : "flex h-screen overflow-hidden"}>
+      <div className="flex h-screen w-full bg-gray-100 text-black dark:bg-gray-900 dark:text-white overflow-hidden">
+        <Sidebar darkMode={darkMode} />
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Topbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <MainContent darkMode={darkMode} />
+
       </div>
+    </div>
     </div>
   );
 }
