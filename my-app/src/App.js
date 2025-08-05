b7de70e71f9e5ee48771779d0c5b837e9467e5b3
fernandesjasmine <160141215+fernandesjasmine@users.noreply.github.com>
2025-08-06 00:26:09 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SidebarProvider } from './context/SidebarContext';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './components/Dashboard';
+import Reportspage from './pages/Reportspage';
 import MasterTablesPage from './pages/MasterTablesPage';
 import LoginHistory from './pages/LoginHistory';
 import AdminActivityLog from './pages/AdminActivityLog';
@@ -27,8 +28,10 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/>} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/masterTables" element={<MasterTablesPage />} />
+        <Route path="/reportsPage" element={<Reportspage />} />
         <Route path="/loginHistory" element={<LoginHistory/>}/>
         <Route path="/adminActivity" element={<AdminActivityLog/>}/>
         <Route path="/userActivity" element={<UserActivityLog/>}/>
