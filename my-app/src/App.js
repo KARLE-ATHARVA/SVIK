0da178497f5fb9d0eb267cf59d9d042995df8ac5
fernandesjasmine <160141215+fernandesjasmine@users.noreply.github.com>
2025-10-08 20:20:10 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SidebarProvider } from './context/SidebarContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css'; 
+
 import LoginPage from './pages/LoginPage';
 import Dashboard from './components/Dashboard';
+import Reportspage from './pages/Reportspage';
+import MasterTablesPage from './pages/MasterTablesPage';
 import LoginHistory from './pages/LoginHistory';
 import AdminActivityLog from './pages/AdminActivityLog';
 import UserActivityLog from './pages/UserActivityLog';
@@ -15,32 +23,50 @@ import SpaceMasterPage from './pages/SpaceMasterPage';
 import FinishMasterPage from './pages/FinishMasterPage';
 import UserMasterPage from './pages/UserMasterPage';
 import CompanyMasterPage from './pages/CompanyMasterPage';
-import LoginMasterPage from './pages/LoginMasterPage';
 import PlanMasterPage from './pages/PlanMasterPage';
+import RegisterPage from './pages/RegisterPage';
+import AddTilePage from './pages/AddTilePage';
+import EditTilePage from './pages/EditTilePage';
+import ProductsImagePage from './pages/ProductsImagePage';
+import ViewTilePage from './pages/ViewTilePage';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/loginHistory" element={<LoginHistory/>}/>
-        <Route path="/adminActivity" element={<AdminActivityLog/>}/>
-        <Route path="/userActivity" element={<UserActivityLog/>}/>
-        <Route path="/sizeMaster" element={<SizeMasterPage />} />
-        <Route path="/applicationMaster" element={<ApplicationMasterPage />} />
-        <Route path="/tileMaster" element={<TileMasterPage />} />
-        <Route path="/profileMaster" element={<ProfileMasterPage />} />
-        <Route path="/colorMaster" element={<ColorMasterPage />} />
-        <Route path="/categoryMaster" element={<CategoryMasterPage />} />
-        <Route path="/spaceMaster" element={<SpaceMasterPage />} />
-        <Route path="/finishMaster" element={<FinishMasterPage />} />
-        <Route path="/userMaster" element={<UserMasterPage />} />
-        <Route path="/companyMaster" element={<CompanyMasterPage />} />
-        <Route path="/loginMaster" element={<LoginMasterPage />} />
-        <Route path="/planMaster" element={<PlanMasterPage />} />
+    <ThemeProvider>
+      <SidebarProvider>
+        <Router>
+          {/* ToastContainer added at root so all components can use toast */}
+          <ToastContainer 
+            
+          />
 
-      </Routes>
-    </Router>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/masterTables" element={<MasterTablesPage />} />
+            <Route path="/reportsPage" element={<Reportspage />} />
+            <Route path="/loginHistory" element={<LoginHistory />} />
+            <Route path="/adminActivity" element={<AdminActivityLog />} />
+            <Route path="/userActivity" element={<UserActivityLog />} />
+            <Route path="/sizeMaster" element={<SizeMasterPage />} />
+            <Route path="/applicationMaster" element={<ApplicationMasterPage />} />
+            <Route path="/tileMaster" element={<TileMasterPage />} />
+            <Route path="/tileImage" element={<ProductsImagePage />} />
+            <Route path="/profileMaster" element={<ProfileMasterPage />} />
+            <Route path="/colorMaster" element={<ColorMasterPage />} />
+            <Route path="/categoryMaster" element={<CategoryMasterPage />} />
+            <Route path="/spaceMaster" element={<SpaceMasterPage />} />
+            <Route path="/finishMaster" element={<FinishMasterPage />} />
+            <Route path="/userMaster" element={<UserMasterPage />} />
+            <Route path="/companyMaster" element={<CompanyMasterPage />} />
+            <Route path="/planMaster" element={<PlanMasterPage />} />
+            <Route path="/add-tile" element={<AddTilePage />} />
+            <Route path="/edit-tile/:tileId" element={<EditTilePage />} />
+            <Route path="/view-tile/:tileId" element={<ViewTilePage />} />
+          </Routes>
+        </Router>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
